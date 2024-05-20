5d11f4096f36ae64d48ede8ea045ee0c8f84bc1e
Thet Wai Aung <126937793+thetwaiAung-Developer@users.noreply.github.com>
2024-05-20 15:33:32 +0630

    [Complete] redis connection and using redis

---
+++ ConnectionHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.IdentityModel.Protocols;
+using StackExchange.Redis;
+
+namespace RedisDotNet7API
+{
+    public class ConnectionHelper
+    {
+        static ConnectionHelper()
+        {
+            ConnectionHelper.lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            });
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection;
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+}
